46f88aa396b1a3ec73a91f515f112fe1d95736ad
dimonsuvorov991-ai <dimonsuvorov991@gmail.com>
2026-03-23 16:37:12 +0300

    main.py

--- main.py
+++ main.py
@@ -1,41 +1,12 @@
- if len(media_urls) > 1:
-        for i, media in enumerate(media_urls):
-            try:
-                if media.endswith(".mp4"):
-                    bot.send_video(chat_id=CHAT_ID, video=media, caption=text if i == 0 else "")
-                else:
-                    bot.send_photo(chat_id=CHAT_ID, photo=media, caption=text if i == 0 else "")
-                time.sleep(2)
-            except Exception as e:
-                print(e)
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-    # если одно медиа
-    elif len(media_urls) == 1:
-        media = media_urls[0]
-        try:
-            if media.endswith(".mp4"):
-                bot.send_video(chat_id=CHAT_ID, video=media, caption=text)
-            else:
-                bot.send_photo(chat_id=CHAT_ID, photo=media, caption=text)
-        except:
-            bot.send_message(chat_id=CHAT_ID, text=text)
+TOKEN = "ТВОЙ_ТОКЕН"
 
-    # если нет медиа
-    else:
-        bot.send_message(chat_id=CHAT_ID, text=text)
+async def get_id(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print("CHAT ID:", update.effective_chat.id)
 
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, get_id))
 
-while True:
-    feed = feedparser.parse(RSS_URL)
-
-    for entry in reversed(feed.entries):
-        if entry.link not in posted:
-            try:
-                send_post(entry)
-                posted.add(entry.link)
-                time.sleep(5)
-            except Exception as e:
-                print(e)
-
-    time.sleep(30)
-
+app.run_polling()
